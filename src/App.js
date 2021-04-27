fa33aa1a2ced8b87a5883029261f6529af8e835e
J.Laro <joselarojr@gmail.com>
2021-04-27 15:55:21 -0400

    redux store & provider setup

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import React, {Fragment, useEffect} from 'react'
-import 'materialize-css/dist/css/materialize.min.css';
-import M from 'materialize-css/dist/js/materialize.min.js';
-import './App.css';
-import SearchBar from './components/layout/SearchBar';
-import Logs from './components/logs/Logs';
-import AddBtn from './components/layout/AddBtn';
-import AddLogModal from './components/logs/AddLogModal';
-import EditLogModal from './components/logs/EditLogModal';
-import AddTechModal from './components/techs/AddTechModal';
-import TechListModal from './components/techs/TechListModal';
-
+import React, { Fragment, useEffect } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
+import {
+  SearchBar,
+  Logs,
+  AddBtn,
+  AddLogModal,
+  EditLogModal,
+  AddTechModal,
+  TechListModal,
+} from "./components";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   useEffect(() => {
     //Init Materialize JS
     M.AutoInit();
-  })
+  });
   return (
-    <Fragment>
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export {default as SearchBar} from './layout/SearchBar';
+export {default as Logs} from './logs/Logs';
+export {default as AddBtn} from './layout/AddBtn';
+export {default as AddLogModal} from './logs/AddLogModal';
+export {default as EditLogModal} from './logs/EditLogModal';
+export {default as AddTechModal} from './techs/AddTechModal';
+export {default as TechListModal} from './techs/TechListModal';
