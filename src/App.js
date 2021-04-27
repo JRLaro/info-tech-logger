b57cd7cd2ea35bcf2a86b242eab85e23032bdda7
J.Laro <joselarojr@gmail.com>
2021-04-27 16:41:16 -0400

    logs reducer, action types

--- src/App.js
+++ src/App.js
@@ -2,15 +2,7 @@ import React, { Fragment, useEffect } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
-import {
-  SearchBar,
-  Logs,
-  AddBtn,
-  AddLogModal,
-  EditLogModal,
-  AddTechModal,
-  TechListModal,
-} from "./components";
+import {SearchBar,Logs,AddBtn,AddLogModal,EditLogModal,AddTechModal,TechListModal,} from "./components";
 import { Provider } from "react-redux";
 import store from "./store";
 
