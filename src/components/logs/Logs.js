f6d36db51f4064f3089566762c10774498720f39
J.Laro <joselarojr@gmail.com>
2021-04-27 13:17:26 -0400

    logitme & preloader

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import LogItem from "./LogItem";
+import PreLoader from "../layout/PreLoader";
 
 const Logs = () => {
   const [logs, setLogs] = useState([]);
@@ -28,15 +30,15 @@ const Logs = () => {
   };
 
   if (loading) {
-    return <h4>Loading...</h4>;
+    return <PreLoader/>;
   }
 
   return (
-    <ul className="collection-with-header">
+    <ul className="collection with-header">
       <li className="collection-header">
         <h4 className="center">System Logs</h4>
           </li>
-          {!loading && logs.length === 0 ? (<p className="center">You have no logs</p>) : (logs.map((logs) => <li>{logs.message}</li>))}
+          {!loading && logs.length === 0 ? (<p className="center">You have no logs</p>) : (logs.map((log) => <LogItem log={log} key={log.id} />))}
     </ul>
   );
 };
